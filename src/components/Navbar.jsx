1b9b46dae91447d3b2331c4bae3444a7fa849392
Amr <amrmo.cs@gmail.com>
2023-02-20 01:52:30 +0200

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { useCart } from "../Context/CartContext";
-import { useDataContext } from "../Context/DataContext";
 import Nav from "./Nav";
 
 export default function Navbar() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Categories from "../components/Categories";
-import Footer from "../components/Footer";
 import Landing from "../components/Landing";
-import Person from "../components/Person";
 import Products from "../components/Products";
 
 export default function Home() {
