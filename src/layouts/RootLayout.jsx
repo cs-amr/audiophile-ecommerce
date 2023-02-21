bef2d788130dc4a64bc3ca1ae5c3a0d840808501
Amr <amrmo.cs@gmail.com>
2023-02-21 15:58:13 +0200

    commit

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Person from "../components/Person";
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function RootLayout() {
   return (
     <div className="root-layout">
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Person />
